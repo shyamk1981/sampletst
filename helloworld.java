ebfddec2c4bf76f714b05d3c6f98eaece5c1fa6a
shyamk1981 <51149777+shyamk1981@users.noreply.github.com>
2019-08-10 13:45:05 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,11 +1,12 @@
- This is a simple Java program. 
-FileName  HelloWorld.java. 
+/* This is a simple Java program. 
+FileName : "HelloWorld.java". */
 class HelloWorld 
 { 
-	 Your program begins with a call to main(). 
-	 Prints Hello, World to the terminal window. 
+	// Your program begins with a call to main(). 
+	// Prints "Hello, World" to the terminal window. 
 	public static void main(String args[]) 
 	{ 
-		System.out.println(Hello, World in future branch); 
+		System.out.println("Hello, World"); 
 	} 
 } 
+
