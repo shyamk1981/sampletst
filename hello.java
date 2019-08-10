3eda8906bb089b76425e67818d842df45558fe2d
shyamk1981 <51149777+shyamk1981@users.noreply.github.com>
2019-08-10 13:09:06 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -6,6 +6,6 @@ class HelloWorld
 	 Prints Hello, World to the terminal window. 
 	public static void main(String args[]) 
 	{ 
-		System.out.println(Hello); 
+		System.out.println(Hello chnage in git hub); 
 	} 
 } 
